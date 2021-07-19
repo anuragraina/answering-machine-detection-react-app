e8e89ec67e4ae36e5ab67535ad522223be1344ec
Anurag Raina <rainakalhan90@gmail.com>
2021-07-19 15:45:49 +0530

    Add structure to speech to text UI

--- src/components/SpeechToText/index.jsx
+++ src/components/SpeechToText/index.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { Container, Grid, Paper, Typography } from '@material-ui/core';
 
 import { symblAppId, symblAppSecret } from '../../config';
 import { startStream, stopStream } from '../../utils/speech-to-text';
+import LiveTranscript from '../LiveTranscript';
+import EventsTimeline from '../EventsTimeline';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(3),
+	},
+}));
 
 function SpeechToText() {
+	const classes = useStyles();
 	const [streams, setStreams] = useState({});
 	console.log(streams);
 
@@ -33,15 +48,34 @@ function SpeechToText() {
 	};
 
 	return (
-		<div>
-			<button onClick={start} disabled={Object.keys(streams).length > 0}>
-				Start
-			</button>
-			<button onClick={stop} disabled={Object.keys(streams).length === 0}>
-				Stop
-			</button>
-		</div>
+		<Container style={{ width: '100%' }}>
+			{/*<Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />*/}
+			<Grid container direction={'row'}>
+				<LiveTranscript transcriptResponse={undefined} />
+			</Grid>
+			<Grid container direction='row' justify='center' alignItems='flex-start' spacing={3}>
+				<Grid item xs={12} sm={6}>
+					<Paper variant={'outlined'} className={classes.paper}>
+						<Typography variant={'h6'} style={{ marginBottom: 15, paddingBottom: 10 }}>
+							Configurations
+						</Typography>
+					</Paper>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<EventsTimeline events={null} />
+				</Grid>
+			</Grid>
+		</Container>
 	);
 }
 
 export default SpeechToText;
+
+{
+	/* <button onClick={start} disabled={Object.keys(streams).length > 0}>
+				Start
+			</button>
+			<button onClick={stop} disabled={Object.keys(streams).length === 0}>
+				Stop
+			</button> */
+}
